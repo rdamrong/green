1f1384199c77534dc089cafbb625f95eb7151920
Damrongsak Reetanon <damrongs@gmail.com>
2017-12-03 20:28:31 +0700

    v6

--- config.inc.php
+++ config.inc.php
@@ -1,8 +1,8 @@
 <?php
-  header="This is Ansible Workshop";
-  footer="Power by MFEC";
-  db="mysql";
-  port="3306";
-  username="root";
-  password="mypassword123";
+  $header="This is Ansible Workshop";
+  $footer="Power by MFEC";
+  $db="mysql";
+  $port="3306";
+  $username="root";
+  $password="mypassword123";
 ?>
